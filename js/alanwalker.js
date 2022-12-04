eaef2a75428bd3381b1ee97e3f2a2a545692eae8
Professor_Ops_099 <103012661+ujjwaldhiman007@users.noreply.github.com>
2022-12-04 23:43:11 +0530

    fix directory bugs on alanwalker.js

--- js/alanwalker.js
+++ js/alanwalker.js
@@ -1,19 +1,19 @@
 let masterPlay = document.getElementById("masterPlay");
 let masterPause = document.getElementById("masterPause");
 // const myProgressBar = document.getElementById("myProgressBar");
-let songElement = new Audio("../alanwalkersongs/1.mp3");
+let songElement = new Audio("./alanwalkersongs/1.mp3");
 let songIndex = 0;
 let songItems = Array.from(document.getElementsByClassName("main-grid-container"));
 
 let songs = [
-    {number:1, views:"23,40,45,567", time:"3:45", songName: "Unity", songPath:"../alanwalkersongs/1.mp3", coverPath:"../alanwalkercovers/cover1.jpeg"},
-    {number:2, views:"2,734,576,12", time:"5:10", songName: "Lost Control", songPath:"../alanwalkersongs/2.mp3", coverPath:"../alanwalkercovers/cover2.jpg"},
-    {number:3, views:"8,512,546,73", time:"3:59", songName: "Alone", songPath:"../alanwalkersongs/3.mp3", coverPath:"../alanwalkercovers/cover3.jpg"},
-    {number:4, views:"12,00,345,67", time:"4:42", songName: "Fadded", songPath:"../alanwalkersongs/4.mp3", coverPath:"../alanwalkercovers/cover4.jpg"},
-    {number:5, views:"14,23,567,78", time:"4:16", songName: "Lily", songPath:"../alanwalkersongs/5.mp3", coverPath:"../alanwalkercovers/cover5.jpg"},
-    {number:6, views:"2,35,456,764", time:"4:19", songName: "On My Way", songPath:"../alanwalkersongs/6.mp3", coverPath:"../alanwalkercovers/cover6.jpg"},
-    {number:7, views:"3,94,567,981", time:"5:01", songName: "Paradise", songPath:"../alanwalkersongs/7.mp3", coverPath:"../alanwalkercovers/cover7.jpg"},
-    {number:8, views:"26,054,753", time:"5:49", songName: "The Spectre", songPath:"../alanwalkersongs/8.mp3", coverPath:"../alanwalkercovers/cover8.jpg"},
+    {number:1, views:"23,40,45,567", time:"3:45", songName: "Unity", songPath:"./alanwalkersongs/1.mp3", coverPath:"./alanwalkercovers/cover1.jpeg"},
+    {number:2, views:"2,734,576,12", time:"5:10", songName: "Lost Control", songPath:"./alanwalkersongs/2.mp3", coverPath:"./alanwalkercovers/cover2.jpg"},
+    {number:3, views:"8,512,546,73", time:"3:59", songName: "Alone", songPath:"./alanwalkersongs/3.mp3", coverPath:"./alanwalkercovers/cover3.jpg"},
+    {number:4, views:"12,00,345,67", time:"4:42", songName: "Fadded", songPath:"./alanwalkersongs/4.mp3", coverPath:"./alanwalkercovers/cover4.jpg"},
+    {number:5, views:"14,23,567,78", time:"4:16", songName: "Lily", songPath:"./alanwalkersongs/5.mp3", coverPath:"./alanwalkercovers/cover5.jpg"},
+    {number:6, views:"2,35,456,764", time:"4:19", songName: "On My Way", songPath:"./alanwalkersongs/6.mp3", coverPath:"./alanwalkercovers/cover6.jpg"},
+    {number:7, views:"3,94,567,981", time:"5:01", songName: "Paradise", songPath:"./alanwalkersongs/7.mp3", coverPath:"./alanwalkercovers/cover7.jpg"},
+    {number:8, views:"26,054,753", time:"5:49", songName: "The Spectre", songPath:"./alanwalkersongs/8.mp3", coverPath:"./alanwalkercovers/cover8.jpg"},
 
     
 ]
@@ -67,7 +67,7 @@ Array.from(document.getElementsByClassName("song-index-play-btn")).forEach((elem
     element.addEventListener('click', (e)=>{
         // console.log(e.target.id);
         index = parseInt(e.target.id)
-        songElement.src = `../alanwalkersongs/${index+1}.mp3`;
+        songElement.src = `./alanwalkersongs/${index+1}.mp3`;
         songElement.play();
         songElement.currentTime = 0
         masterPlay.style.display = "none";
@@ -87,7 +87,7 @@ document.getElementById("next").addEventListener("click", ()=>{
     else{
         index+=1;
     }
-    songElement.src = `../alanwalkersongs/${index+1}.mp3`;
+    songElement.src = `./alanwalkersongs/${index+1}.mp3`;
     songElement.play();
     songElement.currentTime = 0
     masterPlay.style.display = "none";
@@ -103,7 +103,7 @@ document.getElementById("previous").addEventListener("click", ()=>{
     else{
         index -= 1;
     }
-    songElement.src = `../alanwalkersongs/${index+1}.mp3`;
+    songElement.src = `./alanwalkersongs/${index+1}.mp3`;
     songElement.play();
     songElement.currentTime = 0
     masterPlay.style.display = "none";
